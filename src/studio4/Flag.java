ed50237a2695665d5f071bc39027d98323fd838e
Django-WUSTL <django@wustl.edu>
2026-02-18 14:05:34 -0600

    Completed studio

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -2,10 +2,29 @@ package studio4;
 
 import java.awt.Color;
 
+import java.awt.Font;
+
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(0.5, 0.5, 1, 1);
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledRectangle(0.5, 0.5, 0.35, 0.35);
+		double[] xCoords = {0.5, 0.5, 0.85, 0.15};
+		double[] yCoords = {0.85, 0.15, 0.5, 0.5};
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledPolygon(xCoords, yCoords);
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledCircle(0.325, 0.325, 0.1625);
+		StdDraw.filledCircle(0.675, 0.675, 0.1625);
+		StdDraw.setPenColor(Color.YELLOW);
+		Font font = new Font("Unageo", Font.BOLD, 80);
+		StdDraw.setFont(font);
+		StdDraw.setPenRadius(0.25);
+		StdDraw.text(0.325, 0.325, "π");
+		StdDraw.text(0.675, 0.675, "π");
+
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,50 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+
+		String shape = in.next();
+		System.out.println(shape);
+		int rColorValue = in.nextInt();
+		int gColorValue = in.nextInt();
+		int bColorValue = in.nextInt();
+		StdDraw.setPenColor(rColorValue, gColorValue, bColorValue);
+		boolean isFilled = in.nextBoolean();
+		
+		if(shape.equals("rectangle")){
+			double xValue = in.nextDouble();
+			double yValue = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			if(isFilled){
+				StdDraw.filledRectangle(xValue, yValue, halfWidth, halfHeight);
+			} else{
+				StdDraw.rectangle(xValue, yValue, halfWidth, halfHeight);
+			}
+		} else if(shape.equals("triangle")){
+			double xOne = in.nextDouble();
+			double yOne = in.nextDouble();
+			double xTwo = in.nextDouble();
+			double yTwo = in.nextDouble();
+			double xThree = in.nextDouble();
+			double yThree = in.nextDouble();
+			double[] xCoords = {xOne, xTwo, xThree};
+			double[] yCoords = {yOne, yTwo, yThree};
+			if(isFilled){
+				StdDraw.filledPolygon(xCoords, yCoords);
+			} else{
+				StdDraw.polygon(xCoords, yCoords);
+			}
+		} else if (shape.equals("ellipse")){
+			double xValue = in.nextDouble();
+			double yValue = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			if(isFilled){
+				StdDraw.filledEllipse(xValue, yValue, halfWidth, halfHeight);
+			} else{
+				StdDraw.ellipse(xValue, yValue, halfWidth, halfHeight);
+			}
+		}
 		
 	}
 }
